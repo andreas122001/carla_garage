53b3cd956b3105ccefd0d8824b03f8c30345cc1d
Andreas Bentzen Winje <andreas.b.winje@ntnu.no>
2024-10-03 17:44:33 +0000

    Added a custom runner script (to make it work with remote carla server)

---
+++ env.sh
@@ -0,0 +1,7 @@
+export CARLA_ROOT=/home/andrebw/carla
+export WORK_DIR=/home/andrebw/carla_garage
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI
+export PYTHONPATH=$PYTHONPATH:${CARLA_ROOT}/PythonAPI/carla
+export SCENARIO_RUNNER_ROOT=${WORK_DIR}/scenario_runner
+export LEADERBOARD_ROOT=${WORK_DIR}/leaderboard
+export PYTHONPATH="${CARLA_ROOT}/PythonAPI/carla/":"${SCENARIO_RUNNER_ROOT}":"${LEADERBOARD_ROOT}":${PYTHONPATH}
---
+++ start_expert.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# This script starts PDM-Lite and the CARLA simulator on a local machine
+
+# Exports
+. ./env.sh
+
+# carla
+export REPETITIONS=1
+export DEBUG_CHALLENGE=0
+
+export PTH_ROUTE=${WORK_DIR}/leaderboard/data/routes_devtest
+
+# Start the carla server
+export HOST="192.168.0.100"
+export PORT=2000
+echo "Connection: $HOST:$PORT"
+
+export TEAM_AGENT=${WORK_DIR}/team_code/data_agent.py # use autopilot.py here to only run the expert without data generation
+export CHALLENGE_TRACK_CODENAME=MAP
+export ROUTES=${PTH_ROUTE}.xml
+export TM_PORT=$((PORT + 3))
+
+export CHECKPOINT_ENDPOINT=${PTH_ROUTE}.json
+export TEAM_CONFIG=${PTH_ROUTE}.xml
+export PTH_LOG='logs'
+export RESUME=1
+export DATAGEN=1
+export SAVE_PATH='logs'
+export TM_SEED=0
+
+echo "Running leaderboard evaluator..."
+# Start the actual evaluation / data generation
+python leaderboard/leaderboard/leaderboard_evaluator_local.py \
+       	--host=${HOST}\
+       	--port=${PORT}\
+       	--traffic-manager-port=${TM_PORT}\
+       	--routes=${ROUTES}\
+       	--repetitions=${REPETITIONS}\
+       	--track=${CHALLENGE_TRACK_CODENAME}\
+       	--checkpoint=${CHECKPOINT_ENDPOINT}\
+       	--agent=${TEAM_AGENT}\
+       	--agent-config=${TEAM_CONFIG}\
+       	--debug=0\
+       	--resume=${RESUME}\
+       	--timeout=2000\
+       	--traffic-manager-seed=${TM_SEED}
