dc83ca114deb36bd957d7e36d8dee44dd4f574ba
Andreas Winje <andrebw@stud.ntnu.no>
2024-11-05 15:18:00 +0100

    made target speed weight calculation also calculate angle weights

--- find_target_speed_and_angle_weights.py
+++ find_target_speed_and_angle_weights.py
@@ -11,23 +11,10 @@ from sklearn.utils.class_weight import compute_class_weight
 import ujson
 from team_code.config import GlobalConfig
 
-
+config = GlobalConfig()
 root = "/cluster/work/andrebw/carla_garage/database/dataset_4hz_2024_10_26/data"
-buckets = GlobalConfig().target_speed_bins
-
-def process_file(filepath):
-    """Reads and processes a single file, extracting target speed and its class."""
-
-    with gzip.open(filepath, mode='rb') as f:
-        measurements = json.loads(f.read())
-        ts = measurements['target_speed']
-        # If we are braking, set class to 0
-        if measurements['brake']:
-            ts_class = 0
-        # Else, we use the class, but add one for 'not brake'
-        else:
-            ts_class = np.digitize(x=ts, bins=buckets) + 1
-        return ts, ts_class
+speed_bins = config.target_speed_bins
+angle_bins = config.angle_bins
 
 def get_measurement_files(root):
     """Generates a list of all measurement files in the given root directory."""
@@ -49,32 +36,50 @@ def get_measurement_files(root):
             measurement_files.append(os.path.join(sub_root, "measurements", file_))
     return measurement_files
 
+
+def process_file(filepath):
+    """Reads and processes a single file, extracting target speed and angle classes."""
+
+    with gzip.open(filepath, mode='rb') as f:
+        measurements = json.loads(f.read())
+        ts = measurements['target_speed']
+        angle = measurements['angle']
+        # If we are braking, set class to 0
+        if measurements['brake']:
+            ts_class = 0
+        # Else, we use the class, but add one for 'not brake'
+        else:
+            ts_class = np.digitize(x=ts, bins=speed_bins) + 1
+        angle_class = np.digitize(x=angle, bins=config.angle_bins)
+
+        return ts_class, angle_class
+
 # Gather all file paths to process
 measurement_files = get_measurement_files(root)
 
 # Use multiprocessing to speed up file processing
-target_speeds = []
 target_speed_classes = []
+angle_classes = []
 
 with ProcessPoolExecutor() as executor:
     futures = [executor.submit(process_file, file) for file in measurement_files]
 
     for future in tqdm(as_completed(futures), total=len(futures), desc="Processing files"):
-        ts, ts_class = future.result()
-        if ts is not None:
-            ts = np.digitize(x=ts, bins=range(24))
-            target_speeds.append(ts)
+        ts_class, angle_class = future.result()
+        if not None in [ts_class, angle_class]:
             target_speed_classes.append(ts_class)
+            angle_classes.append(angle_class)
 
 # Compute statistics and display results
-weights = compute_class_weight(class_weight='balanced', classes=np.unique(target_speed_classes), y=target_speed_classes)
-# print(pd.DataFrame(target_speeds).value_counts())
+speed_weights = compute_class_weight(class_weight='balanced', classes=np.unique(target_speed_classes), y=target_speed_classes)
+angle_weights = compute_class_weight(class_weight='balanced', classes=np.unique(angle_classes), y=angle_classes)
 print(pd.DataFrame(target_speed_classes).value_counts())
+print(pd.DataFrame(angle_classes).value_counts())
 print("    self.target_speed_weights = [")
-for w in weights:
+for w in speed_weights:
     print(f"      {w},")
 print("    ]")
-print(np.unique(target_speed_classes))
-print("Max speed:", np.max(target_speeds))
-plt.plot(np.array(target_speeds))
-plt.savefig("target_speed_distributions.png")
+print("    self.angle_weights = [")
+for w in angle_weights:
+    print(f"      {w},")
+print("    ]")
